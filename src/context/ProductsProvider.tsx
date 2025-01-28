6a9b47770ca8185185b57cfe04719c94c6634b20
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2025-01-28 16:33:15 +0500

    Product provider completed

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { createContext, ReactElement, useState, } from "react"
+import { createContext, ReactElement, useEffect, useState, } from "react"
 
 // type created
 export type ProductType = {
@@ -8,25 +8,28 @@ export type ProductType = {
     price:number,
 }
 
+const initState:ProductType[] = []
+
+
 //initial state with the type assign to ProductType[]
-const initState: ProductType[]= [
-            {
-                "sku":"item0001",
-                "name":"Widget",
-                "price":9.99
-            },
-            { 
-                "sku":"item0021",
-                "name":"Premium Widget",
-                "price":19.99
-            },
-            {   
-                "sku":"item0003",
-                "name":"Deluxe Widget",
-                "price":29.99
-            }
+// const initState: ProductType[]= [
+//             {
+//                 "sku":"item0001",
+//                 "name":"Widget",
+//                 "price":9.99
+//             },
+//             { 
+//                 "sku":"item0021",
+//                 "name":"Premium Widget",
+//                 "price":19.99
+//             },
+//             {   
+//                 "sku":"item0003",
+//                 "name":"Deluxe Widget",
+//                 "price":29.99
+//             }
     
-] 
+// ] 
 
 // Ensurs the type of the the values that will be provided to the the context
 export type UseProductsContextType = {
@@ -50,6 +53,21 @@ type ChildrenType = {
 // Creating Context provider
 export const ProductsProvider = ({children}:ChildrenType): ReactElement =>{
     const [products, SetProducts] = useState<ProductType[]>(initState)
+
+    useEffect(()=>{
+        const fetchProducts = async():Promise<ProductType[]>=>{
+            const data = await fetch('http://localhost:3500/products').then(res=>{
+                return res.json()
+            }).catch(err=>{
+                if(err instanceof Error) console.log(err)
+            })
+            
+            return data
+        }
+
+        fetchProducts().then(products => SetProducts(products))
+    },[])
+
 
     return <ProductContext.Provider value={{products}}>
         {children}
