d7075a535e237db33bbb98b0a59a681d4e159881
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2025-01-28 16:14:44 +0500

    ContextProvider created with React Context and TypeScript

---
+++ src/context/ProductsProvider.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createContext, ReactElement, useState, } from "react"
+
+// type created
+export type ProductType = {
+    sku:string,
+    name:string,
+    price:number,
+}
+
+//initial state with the type assign to ProductType[]
+const initState: ProductType[]= [
+            {
+                "sku":"item0001",
+                "name":"Widget",
+                "price":9.99
+            },
+            { 
+                "sku":"item0021",
+                "name":"Premium Widget",
+                "price":19.99
+            },
+            {   
+                "sku":"item0003",
+                "name":"Deluxe Widget",
+                "price":29.99
+            }
+    
+] 
+
+// Ensurs the type of the the values that will be provided to the the context
+export type UseProductsContextType = {
+    products: ProductType[]
+}
+
+//initContextState
+const initContextState:UseProductsContextType = {
+    products:[]
+} 
+
+//Create Context with a type already created
+const ProductContext = createContext<UseProductsContextType>(initContextState)
+
+//Specify type for children
+type ChildrenType = {
+    children?:ReactElement| ReactElement[]
+}
+
+
+// Creating Context provider
+export const ProductsProvider = ({children}:ChildrenType): ReactElement =>{
+    const [products, SetProducts] = useState<ProductType[]>(initState)
+
+    return <ProductContext.Provider value={{products}}>
+        {children}
+    </ProductContext.Provider>
+
+}
+
+
+export default ProductContext
+
+
